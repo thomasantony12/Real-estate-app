171419078c7cffd63ccae5753353fc3e2f6c3e2e
Thomas antony <thomasantony12042001@gmail.com>
2024-10-13 20:30:11 +0530

    profile update add

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,10 +1,41 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../context/AuthContext";
 import "./profileUpdatePage.scss";
+import apiRequest from "../../lib/apiRequest";
+import UploadWidget from "../../components/uploadWidget/UploadWidget";
+import { useNavigate } from "react-router-dom";
+
+
 
 function ProfileUpdatePage() {
+  const {currentUser, updateUser} = useContext(AuthContext);
+
+  console.log(currentUser.avatar);
+  const [error, setError] = useState("");
+  const [avatar, setAvatar] = useState(currentUser.avatar);
+  const navigate = useNavigate();
+
+  async function submitHandler(e) {
+
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const {username, email, password} = Object.fromEntries(formData);
+
+    try{
+      const res = await apiRequest.put(`/users/${currentUser.id}`, {username , email, password, avatar});
+      updateUser(res.data)
+      setError("");
+      navigate("/profile");
+    }catch(err){
+      console.log(err);
+      setError(err.response.data.message);
+    }
+    
+  }
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={submitHandler}>
           <h1>Update Profile</h1>
           <div className="item">
             <label htmlFor="username">Username</label>
@@ -12,6 +43,7 @@ function ProfileUpdatePage() {
               id="username"
               name="username"
               type="text"
+              defaultValue={currentUser.uname}
             />
           </div>
           <div className="item">
@@ -20,17 +52,26 @@ function ProfileUpdatePage() {
               id="email"
               name="email"
               type="email"
+              defaultValue={currentUser.email}
             />
           </div>
           <div className="item">
             <label htmlFor="password">Password</label>
             <input id="password" name="password" type="password" />
           </div>
+          {error && <span>{error}</span>}
           <button>Update</button>
         </form>
       </div>
       <div className="sideContainer">
-        <img src="" alt="" className="avatar" />
+        <img src={avatar || "noavatar.jpg"} alt="" className="avatar" />
+        <UploadWidget uwConfig={{
+          cloudName : "thomas2001",
+          uploadPreset:"estate",
+          multiple:false,
+          maxImageFileSize:2000000,
+          folder:"avatars"
+        }} setAvatar={setAvatar}/>
       </div>
     </div>
   );
